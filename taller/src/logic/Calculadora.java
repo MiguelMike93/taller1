731f6785b57fc2a6a19b3aa024d6b3f6fe89957f
Miguel Beltran <miguel.beltran01@uptc.edu.co>
2017-03-23 11:40:41 -0500

    linea base de calculadora

---
+++ taller/src/logic/Calculadora.java
@@ -0,0 +1,15 @@
+package logic;
+
+public class Calculadora {	
+	public Calculadora() {
+		
+	}
+	
+	public int suma(int num1, int num2){
+		return num1+num2;		
+	}
+	public int resta(int num1, int num2){
+		return num1-num2;
+	}
+	
+}
---
+++ taller/src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+import logic.Calculadora;
+
+public class Main {
+	public static void main(String[] args) {
+		Calculadora calc = new Calculadora();
+		calc.suma(1, 2);
+	}
+}
